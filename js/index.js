278c1e7956eaf94e2f389bcac3b2aab966e10f38
đỗ đạt <dat.hr.neu@gmail.com>
2022-02-12 20:36:28 +0700

    demo

---
+++ js/index.js
@@ -0,0 +1,50 @@
+
+const APP_ID = '816fe009dcdc34b2dc92cf44c33ea6d9'
+const DEFAULT_VALUE = '--'
+const searhInput = document.querySelector('#search-input')
+
+
+const cityName = document.querySelector('.city-name')
+const weatherState = document.querySelector('.wheather-state')
+const weatherIcon = document.querySelector('.weather-icon')
+const temperature = document.querySelector('.temperature')
+
+const sunrise = document.querySelector('.sunrise')
+const sunset = document.querySelector('.sunset')
+const humidity = document.querySelector('.humidity')
+const windSpeed = document.querySelector('.wind-speed')
+
+
+
+
+searhInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
+        .then( async response => {
+            const data = await response.json() 
+            cityName.innerHTML = data.name || DEFAULT_VALUE
+            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE
+            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE
+            humidity.innerHTML =data.main.humidity || DEFAULT_VALUE
+            windSpeed.innerHTML = data.wind.speed || DEFAULT_VALUE
+
+            console.log(data)
+        })
+})
+
+(fetch(`https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=816fe009dcdc34b2dc92cf44c33ea6d9&units=metric&lang=vi`)
+.then( async response => {
+    const data = await response.json() 
+    cityName.innerHTML = data.name || DEFAULT_VALUE
+    weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE
+    weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+    temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE
+    sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE
+    sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE
+    humidity.innerHTML =data.main.humidity || DEFAULT_VALUE
+    windSpeed.innerHTML = data.wind.speed || DEFAULT_VALUE
+
+    console.log(data)
+}))()
